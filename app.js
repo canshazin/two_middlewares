bb5469e7528d687e86d0a34b48af2ef672ce1962
canshazin <shazinshazz@gmail.com>
2024-04-26 12:00:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
+app.use((req, res, next) => {
+  res.send("second middleware");
+});
+app.listen(3000);
